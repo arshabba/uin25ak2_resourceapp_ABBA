b2fe64d8e586e70b1601850c05a66d2abb4d4ee7
Arshad Abba <arshadshekel@gmail.com>
2025-03-14 21:00:00 +0100

    Glemte CSS ved aktiv tab. Måtte trikse litt.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,33 @@
 import "../styles/nav.scss";
 import { resources } from "../assets/ressurser.js";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; 
 
 export default function Nav() {
+  const location = useLocation(); 
+
   return (
     <header>
       <nav>
         <ul>
-          {resources.map((item, index) => (
-            <li key={index}>
-              <Link to={`/${item.category.split(' ')[0].toLowerCase()}`}>
-                {item.category}
-              </Link>
-            </li>
-          ))}
+          {resources.map((item, index) => {
+            const linkPath = `/${item.category.split(' ')[0].toLowerCase()}`;
+            const isActive = location.pathname === linkPath; 
+
+            return (
+              <li key={index}
+              style={{
+                backgroundColor: isActive ? "white" : "var(--morkeblaa)", 
+                color: isActive ? "black !important" : "white", 
+              }}>
+                <Link
+                  to={linkPath}
+                  className={isActive ? "active" : ""} 
+                >
+                  {item.category}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
